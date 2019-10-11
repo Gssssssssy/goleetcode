f2cbf58d3629faca483ffe5f7235aff431e57b76
GSY <coder.gsy@gmail.com>
2019-10-11 13:58:57 +0800

    add l64 codes.

--- src/l64/solution.go
+++ src/l64/solution.go
@@ -6,6 +6,28 @@ package l64
 说明：每次只能向下或者向右移动一步。
 */
 
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
 func minPathSum(grid [][]int) int {
-	return 0
+	rows := len(grid)
+	cols := len(grid[0])
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if i == 0 && j == 0 {
+				continue
+			} else if i != 0 && j == 0 {
+				grid[i][j] = grid[i-1][j] + grid[i][j]
+			} else if i == 0 && j != 0 {
+				grid[i][j] = grid[i][j-1] + grid[i][j]
+			} else {
+				grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j]
+			}
+		}
+	}
+	return grid[rows-1][cols-1]
 }
